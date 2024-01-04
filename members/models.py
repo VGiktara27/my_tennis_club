513da1dfea7965fa5fdee99379e9437999831771
VGiktara27 <155515155+VGiktara27@users.noreply.github.com>
2024-01-04 18:48:15 +0530

    certain modification

--- members/models.py
+++ members/models.py
@@ -2,10 +2,21 @@ from django.db import models
 
 
 class Member(models.Model):
-    firstname = models.CharField(max_length=255)
-    lastname = models.CharField(max_length=255)
+    author_id = models.IntegerField(max_length=255, primary_key=True),
+    firstname = models.CharField(max_length=255, null=False)
+    lastname = models.CharField(max_length=255, null=False)
+    phone_number = models.CharField(max_length=255, null=False)
+    email = models.CharField(max_length=255, null=False)
 
 
 class ShowTable(models.Model):
     Date = models.DateField
     CIF = models.IntegerField
+
+
+class Books(models.Model):
+    book_id = models.IntegerField(max_length=255, primary_key=True),
+    bookname = models.CharField(max_length=255),
+    authorname = models.CharField(max_length=255, null=False),
+    author_id = models.ForeignKey(Member, null=True, on_delete=models.CASCADE).IntegerField(max_length=255, null=False),
+    publication_name = models.CharField(max_length=255, null=False)
